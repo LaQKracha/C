26a4950921578c5ba8cd94edd447b58a4b1edf2a
sa1t <68617409+sa1tty@users.noreply.github.com>
2024-06-28 23:09:27 -0600

    Update ioFileOutput.c

--- File_I-O/ioFileOutput.c
+++ File_I-O/ioFileOutput.c
@@ -1,27 +1,41 @@
-#include <linux/limits.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 
+struct __attribute__((__packed__)) header {
+	unsigned short version;
+	unsigned short nom;
+	unsigned int filesize;
+};
+
 int main(int argc, char *argv[]){
-        if (argc != 2) {
-                printf("Usage: %s <filename>\n", argv[0]);
-                return 0;
-        }
+	struct header head = {0};
+	if (argc != 2) {
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 0;
+	}
 
-        // define file descriptor (man 2 open)
-        // function params -> open(filepath, access_mode, permisions_in_case_of_new_file)  
-        int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
-        if (fd == -1) {
-                perror("open");
-                return -1;
-        }
+	int fd = open(argv[1], O_RDWR | O_CREAT, 0664);
+	if (fd == -1) {
+		perror("open");
+		return -1;
+	}
 
-        char *buf = "I'm a content created by C code ,)";
-        write(fd, buf, strlen(buf));
-        close(fd);
+	char *buff = "Hello, I'm the default content\n";
+	write(fd, buff, strlen(buff));
+	close(fd);
+
+	FILE *file;
+	file = fopen(argv[1], "a+");
+	if (file == NULL) {
+		perror("fopen");
+		return -1;
+	}
+	
+	fprintf(file, "And something else added\n");
+	fclose(file);
 
 
-        return 0;
+	return 0;
 }
